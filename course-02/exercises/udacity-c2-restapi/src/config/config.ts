7f62c767ad8cd521a1d46569ec8308c61be694f2
HanzoHan <hanzohanic@gmail.com>
2022-08-25 02:22:59 +0200

    v1

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "black1996",
+    "database": "TestingUda",
+    "host": "database-2.cixcybaehhvl.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "",
+    "aws_profile": "",
+    "aws_media_bucket": ""
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
